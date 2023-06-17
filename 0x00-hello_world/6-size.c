3f44de1486add7ccc5d9594661f87eca3a265948
george <georgemillen42@gmail.com>
2023-06-17 10:33:44 -0700

    types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints different types
+ *
+ * Return: 0 should be th return
+ */
+int main(void)
+{
+printf("size of a char : %d byte(s)\n", size of(char));
+printf("size of an int : %d byte(s)\n", size of(int));
+printf("size of long int : %d byte(s)\n", size of(long int));
+printf("size of	a longlong int : %d byte(s)\n", size of(long long int));
+printf("size of a float : %d byte(s)\n", size of(float));
+return (0);
+}
