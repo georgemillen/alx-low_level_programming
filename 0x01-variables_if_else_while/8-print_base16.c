ed8bd1160c5eba5ef47444fff1939339106c0ade
george <georgemillen42@gmail.com>
2023-06-18 05:22:00 -0700

    hexs

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
