a99b2ebe5cfd0ca872c0b8c277325715d306a0e1
george <georgemillen42@gmail.com>
2023-06-18 02:27:12 -0700

    Aa

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,13 @@
  */
 int main(void)
 {
-	char Alph[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
-	int a, i;
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	for (a = 0; a < 26; a++)
+	for (i = 0; i < 52; i++)
 	{
-	for (i = 0; i < 26; i++)
-		{
-	putchar(alph[a][i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
-		}
 	return (0);
 }
