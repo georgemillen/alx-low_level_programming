0a074722eeeac77c247423e0bb3bfb19acf9299f
george <georgemillen42@gmail.com>
2023-06-22 07:01:18 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_word - prints a word
+ * @i: number of char in word
+ * return: void
+ */
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_word - prints a word
+ * @word: word to print
+ * return: void
+ */
+void print_word(const char* word)
+{
+        int i = 0;
+        while (word[i] != '\0')
+        {
+                putchar(word[i]);
+                i++;
+                }
+}
+
+int main()
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+_putchar(i);
+else{
+        for (i % 3 == 0)
+        const char *word = "Fizz"
+        print_word (word);
+        _putchar(' ');
+                        }
+else if{
+        for (i % 5 == 0)
+        const char *word = "Buzz"
+        print_word (word);
+        _putchar (' ');
+        }
+else if{
+        for ( i % 3 == 0 && i % 5 == 0)
+        const char *word = "FizzBuzz"
+        print_word (word);
+        _putchar(' ');
+        }
+        putchar(' ');
+}
